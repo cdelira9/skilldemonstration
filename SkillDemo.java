97edbfcce31dc3a4d50d9027b2a787329faa9164
Cynthia <cdelira@ucsd.edu>
2022-02-04 15:38:46 -0800

    lib + incorrect test

--- SkillDemo.java
+++ SkillDemo.java
@@ -1,1 +1,5 @@
-public class SkillDemo{}
+public class SkillDemo{
+    public static int remainder(int a, int b){
+        return a%b;
+    }
+}
---
+++ SkillDemoTest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemoTest {
+    @Test
+    public void remainders() {
+        assertEquals(0, SkillDemo.remainder(5,2));
+    }
+}
